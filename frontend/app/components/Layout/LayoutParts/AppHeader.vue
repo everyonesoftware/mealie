46d96e5484e4884b01e08c8f05d1ff81d0f8d215
Dan Schulte <dan.schulte.qub@live.com>
2026-07-03 12:13:01 -0700

    feat: Remove dummy change

--- frontend/app/components/Layout/LayoutParts/AppHeader.vue
+++ frontend/app/components/Layout/LayoutParts/AppHeader.vue
@@ -25,7 +25,7 @@
         style="cursor: pointer"
         @click="$router.push(routerLink)"
       >
-        TrailFood Library (I think?)
+        TrailFood Library
       </v-toolbar-title>
     </div>
     <RecipeDialogSearch ref="domSearchDialog" />
